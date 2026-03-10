c4cc1a4bb28a15597f81df79009aa6fc4c3a7ddf
miozhou <zhoumiao1004@gmail.com>
2026-03-10 08:57:50 +0800

    add presum

--- 1-array/main.go
+++ 1-array/main.go
@@ -328,71 +328,6 @@ func search2(nums []int, target int) int {
 	return -1
 }
 
-// 74.搜索二维矩阵
-// https://leetcode.cn/problems/search-a-2d-matrix/
-// 给你一个满足下述两条属性的 m x n 整数矩阵：
-// 每行中的整数从左到右按非严格递增顺序排列。
-// 每行的第一个整数大于前一行的最后一个整数。
-// 给你一个整数 target ，如果 target 在矩阵中，返回 true ；否则，返回 false 。
-// 输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
-// 输出：true
-func searchMatrix(matrix [][]int, target int) bool {
-	m, n := len(matrix), len(matrix[0])
-	// 纵向二分,找左边界
-	left, right := 0, m-1
-	for left <= right {
-		mid := (left + right) / 2
-		if matrix[mid][0] > target {
-			right = mid - 1
-		} else if matrix[mid][0] < target {
-			left = mid + 1
-		} else {
-			return true
-		}
-	}
-	// 第一列没找到：right指向的第一个小于target的位置
-	fmt.Println(left, right)
-	if right < 0 {
-		right++
-	}
-	// 横向二分
-	row := right
-	left, right = 0, n-1
-	for left <= right {
-		mid := (left + right) / 2
-		if matrix[row][mid] == target {
-			return true
-		} else if matrix[row][mid] < target {
-			left = mid + 1
-		} else {
-			right = mid - 1
-		}
-	}
-	return false
-}
-
-// 240.搜索二维矩阵 II
-// https://leetcode.cn/problems/search-a-2d-matrix-ii/
-// 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
-// 每行的元素从左到右升序排列。
-// 每列的元素从上到下升序排列。
-// 输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
-// 输出：true
-func searchMatrix2(matrix [][]int, target int) bool {
-	m, n := len(matrix), len(matrix[0])
-	i, j := m-1, 0
-	for i >= 0 && j < n {
-		if matrix[i][j] == target {
-			return true
-		} else if matrix[i][j] < target {
-			j++
-		} else if matrix[i][j] > target {
-			i--
-		}
-	}
-	return false
-}
-
 // 209. 长度最小的子数组
 // https://leetcode.cn/problems/minimum-size-subarray-sum/description/
 // 输入：target = 7, nums = [2,3,1,2,4,3]
@@ -410,51 +345,6 @@ func minSubArrayLen(target int, nums []int) int {
 		}
 	}
 	return result
-}
-
-// 59.螺旋矩阵II
-// https://leetcode.cn/problems/spiral-matrix-ii/
-// 输入: 3 输出: [ [ 1, 2, 3 ], [ 8, 9, 4 ], [ 7, 6, 5 ] ]
-func generateMatrix(n int) [][]int {
-	results := make([][]int, n)
-	for i := 0; i < n; i++ {
-		results[i] = make([]int, n)
-	}
-	loop := n / 2
-	startx, starty := 0, 0
-	offset := 0
-	cnt := 1
-	for loop > 0 {
-		// from left to right
-		i, j := startx, starty
-		for ; j < n-1-offset; j++ {
-			results[i][j] = cnt
-			cnt++
-		}
-		// from top to bottom
-		for ; i < n-1-offset; i++ {
-			results[i][j] = cnt
-			cnt++
-		}
-		// from right to left
-		for ; j > starty; j-- {
-			results[i][j] = cnt
-			cnt++
-		}
-		// from bottom to top
-		for ; i > startx; i-- {
-			results[i][j] = cnt
-			cnt++
-		}
-		offset++
-		startx++
-		starty++
-		loop--
-	}
-	if n%2 == 1 {
-		results[n/2][n/2] = cnt
-	}
-	return results
 }
 
 // 1365. 有多少小于当前数字的数字
@@ -526,25 +416,6 @@ func uniqueOccurrences(arr []int) bool {
 		freq[val] = true
 	}
 	return true
-}
-
-// 283. 移动零
-// https://leetcode.cn/problems/move-zeroes/description/
-// 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
-// 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
-// 输入: nums = [0,1,0,3,12]
-// 输出: [1,3,12,0,0]
-func moveZeroes(nums []int) {
-	left := 0
-	for i := 0; i < len(nums); i++ {
-		if nums[i] != 0 {
-			nums[left] = nums[i]
-			left++
-		}
-	}
-	for ; left < len(nums); left++ {
-		nums[left] = 0
-	}
 }
 
 // 189. 轮转数组
--- 1-array/matrix.go
+++ 1-array/matrix.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // 48. 旋转图像
 // https://leetcode.cn/problems/rotate-image/description/
 // 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
@@ -119,4 +121,69 @@ func generateMatrix(n int) [][]int {
 		result[n/2][n/2] = cnt
 	}
 	return result
+}
+
+// 74.搜索二维矩阵
+// https://leetcode.cn/problems/search-a-2d-matrix/
+// 给你一个满足下述两条属性的 m x n 整数矩阵：
+// 每行中的整数从左到右按非严格递增顺序排列。
+// 每行的第一个整数大于前一行的最后一个整数。
+// 给你一个整数 target ，如果 target 在矩阵中，返回 true ；否则，返回 false 。
+// 输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+// 输出：true
+func searchMatrix(matrix [][]int, target int) bool {
+	m, n := len(matrix), len(matrix[0])
+	// 纵向二分,找左边界
+	left, right := 0, m-1
+	for left <= right {
+		mid := (left + right) / 2
+		if matrix[mid][0] > target {
+			right = mid - 1
+		} else if matrix[mid][0] < target {
+			left = mid + 1
+		} else {
+			return true
+		}
+	}
+	// 第一列没找到：right指向的第一个小于target的位置
+	fmt.Println(left, right)
+	if right < 0 {
+		right++
+	}
+	// 横向二分
+	row := right
+	left, right = 0, n-1
+	for left <= right {
+		mid := (left + right) / 2
+		if matrix[row][mid] == target {
+			return true
+		} else if matrix[row][mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return false
+}
+
+// 240.搜索二维矩阵 II
+// https://leetcode.cn/problems/search-a-2d-matrix-ii/
+// 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+// 每行的元素从左到右升序排列。
+// 每列的元素从上到下升序排列。
+// 输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+// 输出：true
+func searchMatrix2(matrix [][]int, target int) bool {
+	m, n := len(matrix), len(matrix[0])
+	i, j := m-1, 0
+	for i >= 0 && j < n {
+		if matrix[i][j] == target {
+			return true
+		} else if matrix[i][j] < target {
+			j++
+		} else if matrix[i][j] > target {
+			i--
+		}
+	}
+	return false
 }
